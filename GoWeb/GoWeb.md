b57b7aff04a132786321caaa377e387e9d29baa5
Andy Ron <randy.njfu@gmail.com>
2024-07-06 22:15:33 +0800

    go web

---
+++ GoWeb/GoWeb.md
@@ -0,0 +1,59 @@
+Go Web
+---
+
+https://learnku.com/docs/build-web-application-with-golang
+
+
+https://github.com/astaxie/build-web-application-with-golang
+
+
+
+## 2 GO语言基础
+
+`&x`表示x所在地址；`*int`表示对应的指针类型。
+
+
+
+二十五个关键字：
+
+```go
+break    default      func    interface    select
+case     defer        go      map          struct
+chan     else         goto    package      switch
+const    fallthrough  if      range        type
+continue for          import  return       var
+```
+
+- var 和 const 变量和常量申明
+- package 和 import 
+- func 用于定义函数和方法
+- return 用于从函数返回
+- defer 用于类似析构函数
+- go 用于并发
+- select 用于选择不同类型的通讯
+- interface 用于定义接口
+- struct 用于定义抽象数据类型
+- break、case、continue、for、fallthrough、else、if、switch、goto、default 流程
+- chan 用于 channel 通讯
+- type 用于声明自定义类型
+- map 用于声明 map 类型数据
+- range 用于读取 slice、map、channel 数据
+
+
+
+## 3 web基础
+
+
+
+🔖
+
+
+
+## 4 表单
+
+
+
+## 5 访问数据库
+
+
+
---
+++ GoWeb/go-web/ch03/web.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	http.HandleFunc("/", sayHelloName)
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method: ", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, nil)
+		//log.Println(t.Execute(w, nil))
+	} else {
+		err := r.ParseForm() // 解析 url 传递的参数，对于 POST 则解析响应包的主体
+		if err != nil {
+			log.Fatal("ParseForm: ", err)
+		}
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
+}
+
+func sayHelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()       // 解析参数，默认是不会解析的
+	fmt.Println(r.Form) // 这些信息是输出到服务器端的打印信息
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello world!") // 这个写入到 w 的是输出到客户端的
+}
