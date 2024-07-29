07bda63c3f5338c8fa7a9d97737304f7b7825efd
Andy Ron <randy.njfu@gmail.com>
2024-07-29 11:57:14 +0800

    Go语言第一课:并发

--- Go语言第一课/gofirst/ch31/main.go
+++ Go语言第一课/gofirst/ch31/main.go
@@ -6,6 +6,12 @@ import (
 	"time"
 )
 
+/*
+在main goroutine与子goroutine之间建立了一个元素类型为error的channel，
+子goroutine退出时，会将它执行的函数的错误返回值写入这个channel，
+main goroutine可以通过读取channel的值来获取子goroutine的退出状态。
+*/
+
 func spawn(f func() error) <-chan error {
 	c := make(chan error)
 
