47fcfa7842c089524c60153ca4792190f6e3ed4a
Andy Ron <randy.njfu@gmail.com>
2025-01-05 14:00:47 +0800

    update

---
+++ GoWeb/go-web/ch04-1/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	http.HandleFunc("/", sayHelloName)
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func sayHelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() //解析参数，默认是不会解析的
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //获取请求的方法
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, nil)
+	} else {
+		//请求的是登录数据，那么执行登录的逻辑判断
+		r.ParseForm()
+		fmt.Println("username:",r.Form["username"])
+		fmt.Println("password:",r.Form["password"])
+	}
+}
--- gin/gin.md
+++ gin/gin.md
@@ -12,3 +12,6 @@ https://github.com/gin-gonic/gin
 
 https://gin-gonic.com/zh-cn/docs/examples/multiple-template/
 
+
+
+Gin 使用了自定义版本的 [HttpRouter](https://github.com/julienschmidt/httprouter)
