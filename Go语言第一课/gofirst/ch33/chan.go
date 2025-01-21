82b850ee77c8094301b8704e9ae187b2ae5c4587
Andy Ron <randy.njfu@gmail.com>
2025-01-21 20:12:42 +0800

    Go语言第一课

--- Go语言第一课/gofirst/ch33/chan.go
+++ Go语言第一课/gofirst/ch33/chan.go
@@ -1,10 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
-	t1()
+	//t1()
 	//t2()
 	//t3()
 	//t4()
+	t_select()
 }
 
 func t1() {
@@ -31,12 +34,35 @@ func t3() {
 	ch3 := make(chan int, 1)
 	ch3 <- 17 // 向ch3发送一个整型数17
 	ch3 <- 27 // 由于此时ch3中缓冲区已满，再向ch3发送数据也将导致goroutine挂起
+
 }
 
 func t4() {
-	ch1 := make(chan<- int, 1) // 只发送channel类型
-	ch2 := make(<-chan int, 1) // 只接收channel类型
+	//ch1 := make(chan<- int, 1) // 只发送channel类型
+	//ch2 := make(<-chan int, 1) // 只接收channel类型
+	//
+	//<-ch1     // invalid operation: <-ch1 (receive from send-only type chan<- int)
+	//ch2 <- 13 // invalid operation: ch2 <- 13 (send to receive-only type <-chan int)
+}
 
-	<-ch1     // invalid operation: <-ch1 (receive from send-only type chan<- int)
-	ch2 <- 13 // invalid operation: ch2 <- 13 (send to receive-only type <-chan int)
+func t_select() {
+	ch1 := make(chan int, 5)
+	ch2 := make(chan int, 10)
+	ch3 := make(chan int, 2)
+	ch1 <- 3
+	ch1 <- 5
+	ch2 <- 12
+	ch2 <- 14
+	z := 35
+	select {
+	case x := <-ch1:
+		fmt.Println("来自ch1：", x)
+	case y, _ := <-ch2:
+		fmt.Println(y)
+	case ch3 <- z:
+		fmt.Println(<-ch3)
+	default:
+		fmt.Println("没有可用的channel")
+	}
+
 }
