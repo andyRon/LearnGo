a89268329316f29fcf90432b56717a250145c87d
Andy Ron <randy.njfu@gmail.com>
2025-04-26 17:37:52 +0800

    k8s入门

---
+++ Kubernetes入门实战课/k8s_study/ch04/setup.sh
@@ -0,0 +1,13 @@
+RUN apt-get update \
+    && apt-get install -y \
+        build-essential \
+        curl \
+        make \
+        unzip \
+    && cd /tmp \
+    && curl -fSL xxx.tar.gz -o xxx.tar.gz\
+    && tar xzf xxx.tar.gz \
+    && cd xxx \
+    && ./config \
+    && make \
+    && make clean
